7ddfabad8bbd1282246c2b4f9b537ce1204a3470
sorethea <sorethea@gmail.com>
2025-05-10 05:21:34 +0700

    add trip resource

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -38,9 +38,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
+                ChargeCost::class,
                 Widgets\AccountWidget::class,
                 //Widgets\FilamentInfoWidget::class,
-                ChargeCost::class,
+
             ])
             ->plugins([
                 FilamentShieldPlugin::make(),
