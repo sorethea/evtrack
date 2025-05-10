4c0749dc0f7bdcd017fb72427e5d8bbee0cf8d33
sorethea <sorethea@gmail.com>
2025-05-10 15:17:31 +0700

    add trip resource

--- app/Filament/Resources/ChargeResource/Widgets/ChargeOverview.php
+++ app/Filament/Resources/ChargeResource/Widgets/ChargeOverview.php
@@ -6,6 +6,7 @@ use App\Models\Charge;
 use Filament\Widgets\Concerns\InteractsWithPageTable;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\DB;
 
 class ChargeOverview extends BaseWidget
 {
