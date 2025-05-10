36a031450e5d239de48c551e526ac1eecda2f177
sorethea <sorethea@gmail.com>
2025-05-10 21:07:54 +0700

    add trip resource

--- app/Filament/Resources/ChargeResource/Widgets/ChargeOverview.php
+++ app/Filament/Resources/ChargeResource/Widgets/ChargeOverview.php
@@ -3,28 +3,30 @@
 namespace App\Filament\Resources\ChargeResource\Widgets;
 
 use App\Models\Charge;
+use App\Models\DrivingLog;
 use App\Models\Trip;
 use Filament\Widgets\Concerns\InteractsWithPageTable;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Number;
+use Spatie\Color\Distance;
 
 class ChargeOverview extends BaseWidget
 {
     use InteractsWithPageTable;
     protected function getStats(): array
     {
-        $minOdo = Trip::selectRaw('MIN(odo_from) AS min_odo')
-            ->where('date_from','>=',now()->subMonths(12))
+        $minOdo = DrivingLog::selectRaw('MIN(odo) AS min_odo')
+            ->where('date','>=',now()->subMonths(12))
             ->value('min_odo');
-        $maxOdo = Trip::selectRaw('MAX(odo_to) AS max_odo')
-            ->where('date_from','>=',now()->subMonths(12))
+        $maxOdo = DrivingLog::selectRaw('MAX(odo) AS max_odo')
+            ->where('date','>=',now()->subMonths(12))
             ->value('max_odo');
-        $distance = Trip::selectRaw('MAX(odo_to)-MIN(odo_from) AS distance')
-            ->where('date_from','>=',now()->subMonths(12))
+        $distance = DrivingLog::selectRaw('MAX(odo)-MIN(odo) AS distance')
+            ->where('date','>=',now()->subMonths(12))
             ->value('distance');
-        $distanceByMonth = Trip::selectRaw('MAX(odo_to)-MIN(odo_from) AS distance,MONTH(date_from) AS month')
-            ->where('date_from','>=',now()->subMonths(12))
+        $distanceByMonth = DrivingLog::selectRaw('MAX(odo)-MIN(odo) AS distance,MONTH(date) AS month')
+            ->where('date','>=',now()->subMonths(12))
             ->groupBy('month')
             ->pluck('distance');
         $total = Charge::selectRaw('SUM(`qty`*`price`) as `cost`')
