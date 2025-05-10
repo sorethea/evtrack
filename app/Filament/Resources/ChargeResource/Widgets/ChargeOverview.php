ba9cd22cb33e0f6ec6e99abe02514d498f00e290
sorethea <sorethea@gmail.com>
2025-05-10 15:09:16 +0700

    add trip resource

--- app/Filament/Resources/ChargeResource/Widgets/ChargeOverview.php
+++ app/Filament/Resources/ChargeResource/Widgets/ChargeOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\ChargeResource\Widgets;
 
+use App\Models\Charge;
 use Filament\Widgets\Concerns\InteractsWithPageTable;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -11,10 +12,9 @@ class ChargeOverview extends BaseWidget
     use InteractsWithPageTable;
     protected function getStats(): array
     {
+        $total_qty = Charge::sum("qty");
         return [
-            Stat::make("Total Charging Energy", $this->getPageTableQuery()->each(function ($record){
-
-            }))
+            Stat::make("Total Charging Energy", $total_qty)
         ];
     }
 }
