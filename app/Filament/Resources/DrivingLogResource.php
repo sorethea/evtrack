9952b4171bb8bb6f93901640d6700bd8d294c953
sorethea <sorethea@gmail.com>
2025-05-10 23:05:02 +0700

    add trip resource

--- app/Filament/Resources/DrivingLogResource.php
+++ app/Filament/Resources/DrivingLogResource.php
@@ -35,7 +35,7 @@ class DrivingLogResource extends Resource
                         ->default(function (){
                             $maxDate = DrivingLog::max('date');
                             $log = DrivingLog::where("date","=",$maxDate)->first();
-                            return $log;
+                            return json_encode($log);
                         })
                         ->nullable(),
                     Forms\Components\TextInput::make("soc_to")
