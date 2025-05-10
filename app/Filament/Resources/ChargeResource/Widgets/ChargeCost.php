0546e01206a2ef13d66c033f72e118320fb603d5
sorethea <sorethea@gmail.com>
2025-05-10 13:39:08 +0700

    add trip resource

--- app/Filament/Resources/ChargeResource/Widgets/ChargeCost.php
+++ app/Filament/Resources/ChargeResource/Widgets/ChargeCost.php
@@ -129,7 +129,7 @@ class ChargeCost extends ChartWidget
                                     return context.dataset.label + ': {$currency}'+ value.toLocaleString();
                             },
                             footer: function(context){
-                                console.log(context)
+                                console.log(context.dataset.data)
                                 /*if (!context?.chart?.data?.datasets) return '';
 
                                 const dataIndex = context?.dataIndex ?? 0;
