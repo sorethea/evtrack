f66b5001d38a99b24748e84fd85676f8a2a4825a
sorethea <sorethea@gmail.com>
2025-05-10 14:57:17 +0700

    add trip resource

--- app/Filament/Resources/ChargeResource/Pages/ListCharges.php
+++ app/Filament/Resources/ChargeResource/Pages/ListCharges.php
@@ -4,10 +4,12 @@ namespace App\Filament\Resources\ChargeResource\Pages;
 
 use App\Filament\Resources\ChargeResource;
 use Filament\Actions;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCharges extends ListRecords
 {
+    use ExposesTableToWidgets;
     protected static string $resource = ChargeResource::class;
 
     protected function getHeaderActions(): array
--- app/Filament/Resources/ChargeResource/Widgets/ChargeOverview.php
+++ app/Filament/Resources/ChargeResource/Widgets/ChargeOverview.php
@@ -2,15 +2,17 @@
 
 namespace App\Filament\Resources\ChargeResource\Widgets;
 
+use Filament\Widgets\Concerns\InteractsWithPageTable;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class ChargeOverview extends BaseWidget
 {
+    use InteractsWithPageTable;
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make("Total Charging Energy", $this->getPageTableQuery()->sum('qty'))
         ];
     }
 }
