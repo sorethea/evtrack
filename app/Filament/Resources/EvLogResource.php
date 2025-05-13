5e530438a2d4a4bd84d5d2655cf761f786c38ac3
sorethea <sorethea@gmail.com>
2025-05-13 14:33:40 +0700

    import obd2 data

--- app/Filament/Resources/EvLogResource.php
+++ app/Filament/Resources/EvLogResource.php
@@ -150,7 +150,7 @@ class EvLogResource extends Resource
                     ->options(trans('ev.charge_types.options')),
 
             ])
-            ->defaultSort(fn(Builder $query)=>$query->orderBy('date','desc')->orderBy('id','desc'))
+            ->defaultSort(fn(Builder $query)=>$query->orderBy('date','desc')->orderBy('id','desc')->groupBy('parent_id'))
             ->actions([
                 Tables\Actions\EditAction::make(),
             ])
