fc160932c630a0fd12019466a8a7a7853caa39f5
sorethea <sorethea@gmail.com>
2025-05-13 23:50:35 +0700

    import obd2 data

--- app/Filament/Resources/Obd2LogsResource/Pages/ListObd2Logs.php
+++ app/Filament/Resources/Obd2LogsResource/Pages/ListObd2Logs.php
@@ -51,22 +51,22 @@ class ListObd2Logs extends ListRecords
 
                 ])
                 ->action(function (array $data){
-                    $obd2Logs = config('ev.obd2logs');
-                    $log = Obd2Logs::selectRaw('pid,MIN(value) AS value')
-                        ->distinct()
-                        ->whereIn('pid', array_keys($obd2Logs))
-                        ->groupBy('pid')
-                        ->pluck('value','pid')->toArray();
-                    $evLog = new EvLog();
-                    $evLog->date=$data["date"];
-                    $evLog->parent_id=$data["parent_id"];
-                    $evLog->log_type=$data["log_type"];
-                    $evLog->charge_type=$data["charge_type"]??"";
-                    foreach ($obd2Logs as $key=>$value){
-                        $evLog->$value=$log[$key];
-                    }
-
-                   $evLog->save();
+//                    $obd2Logs = config('ev.obd2logs');
+//                    $log = Obd2Logs::selectRaw('pid,MIN(value) AS value')
+//                        ->distinct()
+//                        ->whereIn('pid', array_keys($obd2Logs))
+//                        ->groupBy('pid')
+//                        ->pluck('value','pid')->toArray();
+//                    $evLog = new EvLog();
+//                    $evLog->date=$data["date"];
+//                    $evLog->parent_id=$data["parent_id"];
+//                    $evLog->log_type=$data["log_type"];
+//                    $evLog->charge_type=$data["charge_type"]??"";
+//                    foreach ($obd2Logs as $key=>$value){
+//                        $evLog->$value=$log[$key];
+//                    }
+//
+//                  // $evLog->save();
 
 //                    $drivingLogLastest = DrivingLog::orderBy('date','desc')->first();
 //                    $drivingLog = new DrivingLog();
