a61fa922c33aab55a0d270bbf26373972310e3a3
sorethea <sorethea@gmail.com>
2025-05-14 13:40:43 +0700

    import obd2 data

--- app/Filament/Resources/EvLogResource.php
+++ app/Filament/Resources/EvLogResource.php
@@ -112,8 +112,21 @@ class EvLogResource extends Resource
 //        ")
 //            ->leftJoin('ev_logs as parent','ev_logs.parent_id','=','parent.id');
             //->joinRelationship('parent');
-        return parent::getEloquentQuery()
-            ->joinRelationship("parent");
+        return parent::getEloquentQuery()->newQuery()
+            ->selectRaw("
+        ROUND(odo - COALESCE(parent.odo,0),0) AS trip_distance,
+        CASE
+                WHEN parent.soc IS NOT NULL AND soc > parent.soc
+                THEN soc - parent.soc
+                ELSE 0
+            END as charge,
+            CASE
+                WHEN parent.soc IS NOT NULL AND parent.soc > soc
+                THEN parent.soc - soc
+                ELSE 0
+            END as discharge
+        ")
+            ->leftJoin('ev_logs as parent','ev_logs.parent_id','=','parent.id');
     }
 
 
