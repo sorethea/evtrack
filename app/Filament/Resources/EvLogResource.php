0ccfcc7fea9e563758dba20cf9df99aa07c99f91
sorethea <sorethea@gmail.com>
2025-05-14 13:27:15 +0700

    import obd2 data

--- app/Filament/Resources/EvLogResource.php
+++ app/Filament/Resources/EvLogResource.php
@@ -95,30 +95,9 @@ class EvLogResource extends Resource
                 ])->columns(2)
             ]);
     }
-//    public static function getEloquentQuery(): Builder
-//    {
-//        return parent::getEloquentQuery()->selectRaw("ev_logs.*,
-//        ROUND(ev_logs.odo - COALESCE(parent.odo,0),0) AS trip_distance,
-//        CASE
-//                WHEN parent.soc IS NOT NULL AND ev_logs.soc > parent.soc
-//                THEN ev_logs.soc - parent.soc
-//                ELSE 0
-//            END as charge,
-//            CASE
-//                WHEN parent.soc IS NOT NULL AND parent.soc > ev_logs.soc
-//                THEN parent.soc - ev_logs.soc
-//                ELSE 0
-//            END as discharge
-//        ")
-//            ->leftJoin('ev_logs as parent','ev_logs.parent_id','=','parent.id');
-//    }
-
-
-    public static function table(Table $table): Table
+    public static function getEloquentQuery(): Builder
     {
-        return $table
-            ->query(function (Builder $query){
-                 $query->selectRaw("ev_logs.*,
+        return parent::getEloquentQuery()->selectRaw("ev_logs.*,
         ROUND(ev_logs.odo - COALESCE(parent.odo,0),0) AS trip_distance,
         CASE
                 WHEN parent.soc IS NOT NULL AND ev_logs.soc > parent.soc
@@ -131,8 +110,14 @@ class EvLogResource extends Resource
                 ELSE 0
             END as discharge
         ")
-                    ->leftJoin('ev_logs as parent','ev_logs.parent_id','=','parent.id');
-            })
+            //->leftJoin('ev_logs as parent','ev_logs.parent_id','=','parent.id')
+            ->joinRelationship('parent');
+    }
+
+
+    public static function table(Table $table): Table
+    {
+        return $table
             ->columns([
                 Tables\Columns\TextColumn::make("date")
                     ->date('d M, Y h i')
