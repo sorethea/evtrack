bd7646e62d930fa1e2fb2e2edecc4bf7debbfa1d
sorethea <sorethea@gmail.com>
2025-05-14 15:15:38 +0700

    import obd2 data

--- app/Filament/Resources/Obd2LogsResource/Pages/ListObd2Logs.php
+++ app/Filament/Resources/Obd2LogsResource/Pages/ListObd2Logs.php
@@ -64,7 +64,7 @@ class ListObd2Logs extends ListRecords
                     $evLog->log_type=$data["log_type"];
                     $evLog->charge_type=$data["charge_type"]??"";
                     foreach ($obd2Logs as $key=>$value){
-                        $evLog->$value=$log[$key];
+                        $evLog->$value=$log[$key]??null;
                     }
                     //$evLog->distance = round($evLog->odo - $parent->odo,1);
                     //$evLog->power = round($parent->soc - $evLog->soc,1);
