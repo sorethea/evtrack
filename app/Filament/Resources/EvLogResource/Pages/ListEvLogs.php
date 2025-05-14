e172479a81c6c78c37a7061ac097d9680118d9cd
sorethea <sorethea@gmail.com>
2025-05-14 14:01:26 +0700

    import obd2 data

--- app/Filament/Resources/EvLogResource/Pages/ListEvLogs.php
+++ app/Filament/Resources/EvLogResource/Pages/ListEvLogs.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\EvLogResource\Pages;
 use App\Filament\Resources\EvLogResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListEvLogs extends ListRecords
 {
@@ -15,5 +16,24 @@ class ListEvLogs extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+    protected function getTableQuery(): ?Builder
+    {
+        return parent::getTableQuery()
+            ->selectRaw("
+        ROUND(odo - COALESCE(parent.odo,0),0) AS trip_distance,
+        CASE
+                WHEN parent.soc IS NOT NULL AND soc > parent.soc
+                THEN soc - parent.soc
+                ELSE 0
+            END as charge,
+            CASE
+                WHEN parent.soc IS NOT NULL AND parent.soc > soc
+                THEN parent.soc - soc
+                ELSE 0
+            END as discharge
+        ")
+            ->leftJoin('ev_logs as parent','ev_logs.parent_id','=','parent.id');
+            ->joinRelationship('parent');
     }
 }
