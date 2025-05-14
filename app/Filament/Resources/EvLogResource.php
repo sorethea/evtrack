56c982c4a5da6a820f22b95ba350348831b69954
sorethea <sorethea@gmail.com>
2025-05-14 13:20:18 +0700

    import obd2 data

--- app/Filament/Resources/EvLogResource.php
+++ app/Filament/Resources/EvLogResource.php
@@ -118,7 +118,7 @@ class EvLogResource extends Resource
     {
         return $table
             ->query(function (Builder $query){
-                return $query->selectRaw("ev_logs.*,
+                 $query->selectRaw("ev_logs.*,
         ROUND(ev_logs.odo - COALESCE(parent.odo,0),0) AS trip_distance,
         CASE
                 WHEN parent.soc IS NOT NULL AND ev_logs.soc > parent.soc
