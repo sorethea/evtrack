7b9f8aee5c75ceb1d06da82302f12f6a681b4989
sorethea <sorethea@gmail.com>
2025-05-25 06:11:04 +0700

    add EvLogOverview widget

--- app/Filament/Resources/EvLogResource/Widgets/EvLogOverview.php
+++ app/Filament/Resources/EvLogResource/Widgets/EvLogOverview.php
@@ -27,7 +27,7 @@ class EvLogOverview extends BaseWidget
         $distanceByMonth = EvLog::selectRaw('ev_logs.odo - COALESCE(parent.odo, 0) AS distance,MONTH(ev_logs.date) AS month')
             ->leftJoin('ev_logs as parent', 'ev_logs.parent_id', 'parent.id')
             ->where('ev_logs.date','>=',now()->subMonths(12))
-            ->get();
+            ->get()->toArray();
         $chargeByMonth = EvLog::selectRaw('COUNT(ev_logs.ac - COALESCE(parent.ac, 0)) AS charge_count,SUM(ev_logs.ac - COALESCE(parent.ac, 0)) AS charge,MONTH(ev_logs.date) AS month')
             ->leftJoin('ev_logs as parent', 'ev_logs.parent_id', 'parent.id')
             ->where('ev_logs.log_type','=','charging')
