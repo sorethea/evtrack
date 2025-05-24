799844e0184af98a90c9ba8c6779731708e6638a
sorethea <sorethea@gmail.com>
2025-05-24 10:24:14 +0700

    add EvLogOverview widget

--- app/Filament/Resources/EvLogResource/Widgets/EvLogOverview.php
+++ app/Filament/Resources/EvLogResource/Widgets/EvLogOverview.php
@@ -17,11 +17,11 @@ class EvLogOverview extends BaseWidget
     {
         $minOdo = EvLog::selectRaw('MIN(odo) AS min_odo')
                 ->whereMonth('date',now()->month)
-                ->whereYear('date',now()->month)
+                ->whereYear('date',now()->year)
                 ->value('min_odo');
         $maxOdo = EvLog::selectRaw('MAX(odo) AS max_odo')
             ->whereMonth('date',now()->month)
-            ->whereYear('date',now()->month)
+            ->whereYear('date',now()->year)
             ->value('max_odo');
         $distanceByMonth = EvLog::selectRaw('MAX(odo)-MIN(odo) AS distance,MONTH(date) AS month')
             ->where('date','>=',now()->subMonths(12))
