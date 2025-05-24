652cbb642cd3caa4704df56c1503bb642b4c9553
sorethea <sorethea@gmail.com>
2025-05-24 20:11:56 +0700

    add EvLogOverview widget

--- app/Filament/Resources/EvLogResource/Widgets/EvLogOverview.php
+++ app/Filament/Resources/EvLogResource/Widgets/EvLogOverview.php
@@ -35,7 +35,7 @@ class EvLogOverview extends BaseWidget
             ->groupBy('month')
             ->pluck('charge','charge_count')->toArray();
         $dischargeByMonth = EvLog::selectRaw('
-                SUM(COALESCE(parent.ad, 0) - ev_logs.ad -
+                SUM(ev_logs.ad - COALESCE(parent.ad, 0) -
                 CASE
                     WHEN ev_logs.log_type like \'driving\'
                     THEN ev_logs.ac - parent.ac
