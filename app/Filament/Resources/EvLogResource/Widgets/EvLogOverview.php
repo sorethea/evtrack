9ac5df04ecaff1906780f342a95a0f668e7c638e
sorethea <sorethea@gmail.com>
2025-05-24 10:10:21 +0700

    add EvLogOverview widget

--- app/Filament/Resources/EvLogResource/Widgets/EvLogOverview.php
+++ app/Filament/Resources/EvLogResource/Widgets/EvLogOverview.php
@@ -16,21 +16,18 @@ class EvLogOverview extends BaseWidget
     protected function getStats(): array
     {
         $minOdo = EvLog::selectRaw('MIN(odo) AS min_odo')
-                ->where('date','>=',now()->subMonths(12))
+                ->where('date','=',now()->subMonths(12))
                 ->value('min_odo');
         $maxOdo = EvLog::selectRaw('MAX(odo) AS max_odo')
             ->where('date','>=',now()->subMonths(12))
             ->value('max_odo');
-        $distance = EvLog::selectRaw('MAX(odo)-MIN(odo) AS distance')
-            ->where('date','>=',now()->subMonths(12))
-            ->value('distance');
         $distanceByMonth = EvLog::selectRaw('MAX(odo)-MIN(odo) AS distance,MONTH(date) AS month')
             ->where('date','>=',now()->subMonths(12))
             ->groupBy('month')
             ->pluck('distance');
         $currency = config("ev.currency");
         return [
-            Stat::make("Total Driving Distance",Number::format($distance)."km")
+            Stat::make("Total driving for this month",Number::format(end($distanceByMonth->toArray()))."km")
                 ->description("Odometer start from {$minOdo} to {$maxOdo}")
                 ->icon('heroicon-o-map')
                 ->color('success')
