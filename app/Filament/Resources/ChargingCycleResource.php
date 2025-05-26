b68cb73f0533020b5f05086eafa0fbb31672f712
sorethea <sorethea@gmail.com>
2025-05-27 06:21:26 +0700

    add EvLogOverview widget

--- app/Filament/Resources/ChargingCycleResource.php
+++ app/Filament/Resources/ChargingCycleResource.php
@@ -84,6 +84,7 @@ class ChargingCycleResource extends Resource
     {
         return [
             'index' => Pages\ManageChargingCycles::route('/'),
+            'view' => Pages\ViewChargeCycle::route('/{record}'),
         ];
     }
 }
---
+++ app/Filament/Resources/ChargingCycleResource/Pages/ViewChargeCycle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ChargingCycleResource\Pages;
+
+use App\Filament\Resources\ChargingCycleResource;
+use App\Filament\Resources\EvLogResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewChargeCycle extends ViewRecord
+{
+    protected static string $resource = ChargingCycleResource::class;
+    protected function getHeaderActions(): array
+    {
+        return [
+            //Actions\EditAction::make(),
+        ];
+    }
+}
