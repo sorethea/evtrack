c49095ad5bf8076a1e8402c594eed801dbef0dfc
sorethea <sorethea@gmail.com>
2025-05-27 09:56:58 +0700

    add EvLogOverview widget

--- app/Filament/Resources/ChargingCycleResource.php
+++ app/Filament/Resources/ChargingCycleResource.php
@@ -32,10 +32,10 @@ class ChargingCycleResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('from_date')
-                    ->date('d/m/y')
+                    ->date('d/m/y h:i')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('to_date')
-                    ->date('d/m/y')
+                    ->date('d/m/y h:i')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('from_soc')
                     ->label('From SOC(%)')
