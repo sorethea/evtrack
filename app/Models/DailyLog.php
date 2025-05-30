c90d363621f3b277489d8940dcc3c79008444e5e
sorethea <sorethea@gmail.com>
2025-05-30 10:10:10 +0700

    create daily logs view

--- app/Models/DailyLog.php
+++ app/Models/DailyLog.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DailyLog extends Model
 {
+    protected $table = 'daily_logs_view';
+    protected $guarded =[];
+
+    public $timestamps = false;
+
+    public $incrementing = false;
     public function log(): BelongsTo
     {
         return $this->belongsTo(EvLog::class,'id','id');
