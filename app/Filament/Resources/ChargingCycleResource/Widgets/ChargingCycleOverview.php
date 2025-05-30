23f545676e17e426e39cc3ae14d676f75975999d
sorethea <sorethea@gmail.com>
2025-05-30 20:32:24 +0700

    Charging cycle overview widget

--- app/Filament/Resources/ChargingCycleResource/Widgets/ChargingCycleOverview.php
+++ app/Filament/Resources/ChargingCycleResource/Widgets/ChargingCycleOverview.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\ChargingCycleResource\Widgets;
 
 use App\Models\ChargingCycle;
+use Carbon\Carbon;
 use Filament\Forms\Get;
 use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
@@ -20,11 +21,14 @@ class ChargingCycleOverview extends BaseWidget
     protected function getStats(): array
     {
         $distancesArray = $this->record->children->pluck('distance')->toArray();
+        $from_date = Carbon::parse($this->record->children->min('date'))->format('d M, Y');
+        $to_date = Carbon::parse($this->record->children->max('date'))->format('d M, Y');
 
         return [
             Stat::make('Total distance',Number::format($this->record->distance??0).'km')
                 ->icon('heroicon-o-map')
                 ->color('success')
+                ->description("From {$from_date} to {$to_date}")
                 ->chart($distancesArray),
         ];
     }
