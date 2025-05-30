029e08f68b4179ca6a577fd3b2828212cbd16069
sorethea <sorethea@gmail.com>
2025-05-30 20:04:13 +0700

    Charging cycle overview widget

--- app/Filament/Resources/ChargingCycleResource.php
+++ app/Filament/Resources/ChargingCycleResource.php
@@ -95,7 +95,7 @@ class ChargingCycleResource extends Resource
     {
         return [
             'index' => Pages\ManageChargingCycles::route('/'),
-            //'view' => Pages\ViewChargeCycle::route('/{record}'),
+            'view' =>  Pages\ViewChargingCycle::route('/{record}'),
         ];
     }
 }
---
+++ app/Filament/Resources/ChargingCycleResource/Pages/ViewChargingCycle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ChargingCycleResource\Pages;
+
+use App\Filament\Resources\ChargingCycleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewChargingCycle extends ViewRecord
+{
+    protected static string $resource = ChargingCycleResource::class;
+}
