73c611e7576afbae15cd8b927c3c57b67c789258
sorethea <sorethea@gmail.com>
2025-05-30 16:16:50 +0700

    Charging cycle overview widget

--- app/Filament/Resources/ChargingCycleResource/Widgets/ChargingCycleOverview.php
+++ app/Filament/Resources/ChargingCycleResource/Widgets/ChargingCycleOverview.php
@@ -6,14 +6,20 @@ use Filament\Forms\Get;
 use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Database\Eloquent\Model;
 
 class ChargingCycleOverview extends BaseWidget
 {
     use InteractsWithRecord;
 
+//    public function getRecord(): Model
+//    {
+//        return $this->record;
+//    }
+
     protected function getStats(): array
     {
-        dd($this->getRecord());
+        dd($this->record);
         return [
             Stat::make('Distance','100km')
         ];
