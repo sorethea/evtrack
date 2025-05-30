bcdf6e086ad7fbba642c8e48c4ee411c042b9282
sorethea <sorethea@gmail.com>
2025-05-30 19:57:19 +0700

    Charging cycle overview widget

--- app/Filament/Resources/ChargingCycleResource/Pages/ViewChargeCycle.php
+++ app/Filament/Resources/ChargingCycleResource/Pages/ViewChargeCycle.php
@@ -16,7 +16,7 @@ class ViewChargeCycle extends ViewRecord
     protected function getHeaderWidgets(): array
     {
         return  [
-            ChargingCycleResource\Widgets\ChargingCycleOverview::make(),
+            ChargingCycleResource\Widgets\ChargingCycleOverview::class(),
         ];
     }
     protected function getHeaderActions(): array
