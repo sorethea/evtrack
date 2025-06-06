7b460bc3419e86d869935fe3df0ccef94314fc07
sorethea <sorethea@gmail.com>
2025-06-06 19:40:53 +0700

    Charging cycle overview widget

--- app/Filament/Resources/EvLogResource/Widgets/ChargingCycleOverview.php
+++ app/Filament/Resources/EvLogResource/Widgets/ChargingCycleOverview.php
@@ -14,12 +14,15 @@ class ChargingCycleOverview extends BaseWidget
     {
         $lastChargingCycle = EvLog::where('log_type','charging')->orderBy('date','desc')->first();
         $distance = 0;
+        $discharge = 0;
         foreach ($lastChargingCycle->children as $child){
             $distance +=$child->daily->distance;
+            $discharge +=$child->daily->discharge;
         }
         return [
             Stat::make('Total Charge',Number::format($lastChargingCycle->daily->energy,1).'kWh'),
-            Stat::make('Total distance', Number::format($distance,1).'km')
+            Stat::make('Total distance', Number::format($distance,1).'km'),
+            Stat::make('Total discharge', Number::format($discharge,1).'kWh')
         ];
     }
 }
