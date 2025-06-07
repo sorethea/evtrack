82db2de0ea4a61fbfc20b59e9d786a33a9a1a861
sorethea <sorethea@gmail.com>
2025-06-07 20:03:48 +0700

    Charging cycle overview widget

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -31,7 +31,7 @@ class Vehicle extends Model
     ];
     public function latestLog(): HasOne
     {
-        return $this->hasOne(EvLog::class)->whereRaw('date=Max(date)',);
+        return $this->hasOne(EvLog::class)->lastestOfMany('date');
     }
     public function logs(): HasMany
     {
