442c69c8f994f23c51df760e2dd2092c652c262c
sorethea <sorethea@gmail.com>
2025-06-14 14:41:33 +0700

    Add ev_log_items

--- app/Filament/Resources/EvLogResource/Pages/ListEvLogs.php
+++ app/Filament/Resources/EvLogResource/Pages/ListEvLogs.php
@@ -79,6 +79,7 @@ class ListEvLogs extends ListRecords
                     foreach ($csv->getRecords() as $index=>$record){
                         if($index >=200) break;
                         $item = ObdItem::where('pid',$record[1])->first();
+                        logger($item);
                         if(!empty($item) && $item->id){
                             EvLogItem::firstOrNew([
                                 'log_id'=>$evLog->id,
