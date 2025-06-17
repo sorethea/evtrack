b52d0faac25182f00261a513f2dafb05913d9a21
sorethea <sorethea@gmail.com>
2025-06-17 11:34:01 +0700

    update vehicle overview

--- app/Filament/Resources/EvLogResource/Widgets/VehicleOverview.php
+++ app/Filament/Resources/EvLogResource/Widgets/VehicleOverview.php
@@ -2,7 +2,6 @@
 
 namespace App\Filament\Resources\EvLogResource\Widgets;
 
-use App\Helpers\EvLog;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Number;
@@ -13,10 +12,10 @@ class VehicleOverview extends BaseWidget
     {
         $vehicle = auth()->user()->vehicle;
         $log = $vehicle->latestLog;
-        $odo = evLog::getItemValue($log,1);
-        $soc = evLog::getItemValue($log,11);
-        $ac = evLog::getItemValue($log,19);
-        $ad = evLog::getItemValue($log,20);
+        $odo = \evlog::getItemValue($log,1);
+        $soc = \evlog::getItemValue($log,11);
+        $ac = \evlog::getItemValue($log,19);
+        $ad = \evlog::getItemValue($log,20);
         return [
             Stat::make(trans('ev.odo'),Number::format($odo).'km'),
             Stat::make(trans('ev.soc'),Number::format($soc).'%'),
