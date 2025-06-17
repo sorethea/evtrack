c528e95e9b6b39319b6846d66171bc118524ac9d
sorethea <sorethea@gmail.com>
2025-06-17 09:51:44 +0700

    update vehicle overview

--- app/Filament/Resources/EvLogResource/Widgets/VehicleOverview.php
+++ app/Filament/Resources/EvLogResource/Widgets/VehicleOverview.php
@@ -2,7 +2,7 @@
 
 namespace App\Filament\Resources\EvLogResource\Widgets;
 
-use App\Helpers\EvLogHelper;
+use App\Helpers\EvLog;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Number;
@@ -14,7 +14,7 @@ class VehicleOverview extends BaseWidget
     {
         $vehicle = auth()->user()->vehicle;
         $log = $vehicle->latestLog;
-        $odo = EvLogHelper::getItemValue($log,11);
+        $odo = EvLog::getItemValue($log,11);
         return [
             Stat::make(trans('ev.odo'),Number::format($odo).'km'),
             Stat::make(trans('ev.soc'),Number::format($log->items->where('item_id',11)->value('value')).'%'),
--- app/Helpers/EvLog.php
+++ app/Helpers/EvLog.php
@@ -4,7 +4,7 @@ namespace App\Helpers;
 
 use Illuminate\Database\Eloquent\Model;
 
-class EvLogHelper
+class EvLog
 {
     function getItemValue(Model $evLog, int $item_id):float {
         return $evLog->items->where('item_id',$item_id)->value('value');
