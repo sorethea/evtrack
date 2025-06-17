8e754045d851328dcfac8d7d73e84356ceb18d86
sorethea <sorethea@gmail.com>
2025-06-17 09:52:45 +0700

    update vehicle overview

--- app/Helpers/EvLog.php
+++ app/Helpers/EvLog.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class EvLog
 {
-    function getItemValue(Model $evLog, int $item_id):float {
+    public static function getItemValue(Model $evLog, int $item_id):float {
         return $evLog->items->where('item_id',$item_id)->value('value');
     }
 }
