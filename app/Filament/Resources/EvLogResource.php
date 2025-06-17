11922b18bce157a42e16cea055b07855c8f7c38e
sorethea <sorethea@gmail.com>
2025-06-17 14:34:09 +0700

    update vehicle overview

--- app/Filament/Resources/EvLogResource.php
+++ app/Filament/Resources/EvLogResource.php
@@ -31,6 +31,7 @@ use Symfony\Component\Mime\Encoder\QpContentEncoder;
 class EvLogResource extends Resource
 {
     use Tables\Concerns\InteractsWithTable;
+
     protected static ?string $model = EvLog::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-calendar';
@@ -47,8 +48,8 @@ class EvLogResource extends Resource
                     Forms\Components\Select::make("parent_id")
                         ->live()
                         ->label(trans('ev.parent'))
-                        ->relationship('parent','date')
-                        ->default(fn()=>EvLog::max('date'))
+                        ->relationship('parent', 'date')
+                        ->default(fn() => EvLog::max('date'))
                         ->searchable()
                         ->nullable(),
                     Forms\Components\Select::make("log_type")
@@ -60,9 +61,9 @@ class EvLogResource extends Resource
                     Forms\Components\Select::make("cycle_id")
                         ->reactive()
                         ->label(trans('ev.cycle'))
-                        ->relationship('cycle','date')
-                        ->hidden(fn(Get $get)=>$get("log_type")!="driving")
-                        ->default(fn()=>EvLog::where("log_type","charging")->max('date'))
+                        ->relationship('cycle', 'date')
+                        ->hidden(fn(Get $get) => $get("log_type") != "driving")
+                        ->default(fn() => EvLog::where("log_type", "charging")->max('date'))
                         ->searchable()
                         ->nullable(),
 //                    Forms\Components\TextInput::make("odo")
@@ -77,7 +78,7 @@ class EvLogResource extends Resource
                     Forms\Components\Select::make("charge_type")
                         ->label(trans('ev.charge_types.name'))
                         ->options(trans("ev.charge_types.options"))
-                        ->hidden(fn(Get $get)=>$get("log_type")!="charging")
+                        ->hidden(fn(Get $get) => $get("log_type") != "charging")
                         ->nullable(),
 //                    Forms\Components\Repeater::make('items')
 //                        ->relationship('items')
@@ -133,80 +134,82 @@ class EvLogResource extends Resource
                     ->searchable(),
                 Tables\Columns\TextColumn::make("log_type")
                     ->badge()
-                    ->color(fn(string $state) => match ($state){
-                        'charging'=>'success',
-                        'driving'=>'info',
-                        'packing'=>'warning',
+                    ->color(fn(string $state) => match ($state) {
+                        'charging' => 'success',
+                        'driving' => 'info',
+                        'packing' => 'warning',
                     })
                     ->label(trans('ev.type'))
-                    ->formatStateUsing(fn(string $state):string =>trans("ev.log_types.options.{$state}"))
+                    ->formatStateUsing(fn(string $state): string => trans("ev.log_types.options.{$state}"))
                     ->searchable(),
                 Tables\Columns\TextColumn::make('soc_from')
-                    ->label(trans('ev.soc_from').'(%)')
-                    ->default(fn(Model $record)=>Number::format(\evlog::getParentItemValue($record,11),1)),
+                    ->label(trans('ev.soc_from') . '(%)')
+                    ->default(fn(Model $record) => Number::format(\evlog::getParentItemValue($record, 11), 1)),
                 Tables\Columns\TextColumn::make('soc_to')
-                    ->label(trans('ev.soc_to').'(%)')
-                    ->default(fn(Model $record)=>Number::format(\evlog::getItemValue($record,11),1)),
+                    ->label(trans('ev.soc_to') . '(%)')
+                    ->default(fn(Model $record) => Number::format(\evlog::getItemValue($record, 11), 1)),
                 Tables\Columns\TextColumn::make('soc_derivation')
-                    ->label(trans('ev.soc_derivation').'(%)')
-                    ->default(function(Model $record){
-                        $derivation = $record?->parent?->items?->where('item_id',11)->value('value')-$record?->items?->where('item_id',11)->value('value');
-                        return Number::format($derivation??0,1);
-                        })
+                    ->label(trans('ev.soc_derivation') . '(%)')
+                    ->default(function (Model $record) {
+                        $derivation = $record?->parent?->items?->where('item_id', 11)->value('value') - $record?->items?->where('item_id', 11)->value('value');
+                        return Number::format($derivation ?? 0, 1);
+                    })
                     ->toggleable(),
                 Tables\Columns\TextColumn::make('soc_middle')
-                    ->label(trans('ev.soc_middle').'(%)')
-                    ->default(function(Model $record){
-                        $soc = $record?->items?->where('item_id',11)->value('value');
-                        $ac = $record?->items?->where('item_id',19)->value('value');
-                        $ad = $record?->items?->where('item_id',20)->value('value');
+                    ->label(trans('ev.soc_middle') . '(%)')
+                    ->default(function (Model $record) {
+                        $soc = $record?->items?->where('item_id', 11)->value('value');
+                        $ac = $record?->items?->where('item_id', 19)->value('value');
+                        $ad = $record?->items?->where('item_id', 20)->value('value');
                         $capacity = $record?->vehicle?->capacity;
-                        $middle = $capacity>0?$soc - 100*($ac-$ad)/$capacity:0;
-                        return Number::format($middle??0,1);
+                        $middle = $capacity > 0 ? $soc - 100 * ($ac - $ad) / $capacity : 0;
+                        return Number::format($middle ?? 0, 1);
                     })
                     ->toggleable(),
                 Tables\Columns\TextColumn::make('voltage_spread')
-                    ->label(trans('ev.voltage_spread').'(mlV)')
-                    ->default(function(Model $record){
-                        $highestVoltage = $record?->items?->where('item_id',24)->value('value');
-                        $lowestVoltage = $record?->items?->where('item_id',22)->value('value');
+                    ->label(trans('ev.voltage_spread') . '(mlV)')
+                    ->default(function (Model $record) {
+                        $highestVoltage = $record?->items?->where('item_id', 24)->value('value');
+                        $lowestVoltage = $record?->items?->where('item_id', 22)->value('value');
                         $spread = $highestVoltage - $lowestVoltage;
-                        return Number::format($spread??0,3);
+                        return Number::format($spread ?? 0, 3);
                     })
                     ->badge()
-                    ->color(fn(string $state) => $state<0.1?'success':($state<0.2?'warning':'danger'))
+                    ->color(fn(string $state) => $state < 0.1 ? 'success' : ($state < 0.2 ? 'warning' : 'danger'))
                     ->toggleable(),
                 Tables\Columns\TextColumn::make('distance')
                     ->label(trans('ev.distance'))
-                    ->default(function(Model $record){
-                        $distance = $record?->items?->where('item_id',1)->value('value')-$record?->parent?->items?->where('item_id',1)->value('value');
-                        return Number::format($distance,1);
+                    ->default(function (Model $record) {
+                        $distance = $record?->items?->where('item_id', 1)->value('value') - $record?->parent?->items?->where('item_id', 1)->value('value');
+                        return Number::format($distance, 1);
                     }),
             ])
-            ->defaultGroup(Tables\Grouping\Group::make('cycle.date')->date()->getDescriptionFromRecordUsing(function (Model $record){
-                $cycle = EvLog::find($record->cycle_id);
-                $lastChildId = $cycle->children()->latest('date')->value('id');
-                $lastChild = EvLog::find($lastChildId);
-                $cycleOdo = \evlog::getItemValue($cycle,1);
-                $cycleSoC = \evlog::getItemValue($cycle,11);
-                $cycleAc = \evlog::getItemValue($cycle,19);
-                $cycleAd = \evlog::getItemValue($cycle,20);
-                $lastChildOdo = \evlog::getItemValue($lastChild,1);
-                $lastChildSoC = \evlog::getItemValue($lastChild,11);
-                $lastChildAc = \evlog::getItemValue($lastChild,19);
-                $lastChildAd = \evlog::getItemValue($lastChild,20);
-                $distance = Number::format($lastChildOdo - $cycleOdo,1);
-                $discharge = Number::format($cycleSoC-$lastChildSoC,1);
-                $aCharge = Number::format($lastChildAc-$cycleAc,1);
-                $aDischarge = Number::format($lastChildAd-$cycleAd,1);
-                $aConsumption = Number::format(100*($aDischarge - $aCharge)/$distance,0);
-                return "Total distance: {$distance}km, Discharge: {$discharge}%, Accumulative Charge: {$aCharge}kWh, Discharge: {$aDischarge}kWh, and Consumption: {$aConsumption}kWh/100km";
-            }))
-            ->groupsOnly()
+            ->defaultGroup()
+            ->groups([
+                Tables\Grouping\Group::make('cycle.date')->date()->getDescriptionFromRecordUsing(function (Model $record) {
+                    $cycle = EvLog::find($record->cycle_id);
+                    $lastChildId = $cycle->children()->latest('date')->value('id');
+                    $lastChild = EvLog::find($lastChildId);
+                    $cycleOdo = \evlog::getItemValue($cycle, 1);
+                    $cycleSoC = \evlog::getItemValue($cycle, 11);
+                    $cycleAc = \evlog::getItemValue($cycle, 19);
+                    $cycleAd = \evlog::getItemValue($cycle, 20);
+                    $lastChildOdo = \evlog::getItemValue($lastChild, 1);
+                    $lastChildSoC = \evlog::getItemValue($lastChild, 11);
+                    $lastChildAc = \evlog::getItemValue($lastChild, 19);
+                    $lastChildAd = \evlog::getItemValue($lastChild, 20);
+                    $distance = Number::format($lastChildOdo - $cycleOdo, 1);
+                    $discharge = Number::format($cycleSoC - $lastChildSoC, 1);
+                    $aCharge = Number::format($lastChildAc - $cycleAc, 1);
+                    $aDischarge = Number::format($lastChildAd - $cycleAd, 1);
+                    $aConsumption = Number::format(100 * ($aDischarge - $aCharge) / $distance, 0);
+                    return "Total distance: {$distance}km, Discharge: {$discharge}%, Accumulative Charge: {$aCharge}kWh, Discharge: {$aDischarge}kWh, and Consumption: {$aConsumption}kWh/100km";
+                })
+            ])
             ->filters([
                 Tables\Filters\QueryBuilder::make()
                     ->constraints([
-                       Tables\Filters\QueryBuilder\Constraints\DateConstraint::make('date'),
+                        Tables\Filters\QueryBuilder\Constraints\DateConstraint::make('date'),
                     ]),
                 Tables\Filters\SelectFilter::make('log_type')
                     ->label(trans('ev.log_types.name'))
@@ -216,11 +219,11 @@ class EvLogResource extends Resource
 //                    ->options(trans('ev.charge_types.options')),
 
             ])
-            ->defaultSort(fn(Builder $query)=>$query->orderBy('date','desc')->orderBy('id','desc'))
+            ->defaultSort(fn(Builder $query) => $query->orderBy('date', 'desc')->orderBy('id', 'desc'))
             ->actions([
 
                 Tables\Actions\Action::make('obd_import')
-                    ->visible(fn($record)=>!$record->items()->count('*'))
+                    ->visible(fn($record) => !$record->items()->count('*'))
                     ->icon('heroicon-o-arrow-up-tray')
                     ->label(trans('ODB2'))
                     ->form([
@@ -229,8 +232,8 @@ class EvLogResource extends Resource
                             ->disk('local')
                             ->directory('obd2'),
                     ])
-                    ->action(function (array $data,Model $record){
-                        self::obdImport($data,$record);
+                    ->action(function (array $data, Model $record) {
+                        self::obdImport($data, $record);
                     }),
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make()->hidden(),
@@ -262,25 +265,25 @@ class EvLogResource extends Resource
 
     public static function obdImport(array $data, EvLog $evLog): void
     {
-        $csv = Reader::createFromPath(Storage::path($data['obd_file']),'r');
+        $csv = Reader::createFromPath(Storage::path($data['obd_file']), 'r');
         $csv->setDelimiter(';');
         $obdFile = $data['obd_file'];
-        $obdFileArray =explode("/",$obdFile);
-        $obdFileName =end($obdFileArray);
-        $obdFileNameArray = explode(".",$obdFileName);
+        $obdFileArray = explode("/", $obdFile);
+        $obdFileName = end($obdFileArray);
+        $obdFileNameArray = explode(".", $obdFileName);
         $evLog->update([
             'date' => $obdFileNameArray[0],
-            'obd_file'=>$obdFile,
+            'obd_file' => $obdFile,
         ]);
-        foreach ($csv->getRecords() as $index=>$row){
+        foreach ($csv->getRecords() as $index => $row) {
             //if($index >=200) break;
-            $item = ObdItem::where('pid',$row[1])->first();
-            if(!empty($item) && $item->id && $evLog->id){
-                $latitude = !empty($row[4])?$row[4]:0.0;
-                $longitude = !empty($row[5])?$row[5]:0.0;
+            $item = ObdItem::where('pid', $row[1])->first();
+            if (!empty($item) && $item->id && $evLog->id) {
+                $latitude = !empty($row[4]) ? $row[4] : 0.0;
+                $longitude = !empty($row[5]) ? $row[5] : 0.0;
                 EvLogItem::query()->firstOrCreate(
-                    ['item_id'=>$item->id,'log_id'=>$evLog->id],
-                    ['value'=>$row[2],'latitude'=>$latitude,'longitude'=>$longitude]);
+                    ['item_id' => $item->id, 'log_id' => $evLog->id],
+                    ['value' => $row[2], 'latitude' => $latitude, 'longitude' => $longitude]);
             }
         }
     }
