700530ffb2fe34ec51b4e343fa468542ecd1484a
sorethea <sorethea@gmail.com>
2025-06-18 10:43:35 +0700

    create ev_logs_view

--- database/migrations/2025_06_18_012713_create_ev_logs_view.php
+++ database/migrations/2025_06_18_012713_create_ev_logs_view.php
@@ -13,11 +13,14 @@ return new class extends Migration
     public function up(): void
     {
         DB::statement('CREATE VIEW ev_logs_view AS
+        WITH ev_logs_base AS(
         SELECT
-          l.id AS log_id,
+          l.id,
+          l.parent_id,
+          l.cycle_id,
+          l.date,
           MAX(CASE WHEN li.item_id = 1 THEN li.value END) AS odo,
-          MAX(CASE WHEN li.item_id = 10 THEN li.value END) AS soc,
-          MAX(CASE WHEN li.item_id = 11 THEN li.value END) AS soc_actual,
+          MAX(CASE WHEN li.item_id = 11 THEN li.value END) AS soc,
           MAX(CASE WHEN li.item_id = 19 THEN li.value END) AS ac,
           MAX(CASE WHEN li.item_id = 20 THEN li.value END) AS ad,
           MAX(CASE WHEN li.item_id = 22 THEN li.value END) AS lvc,
@@ -29,7 +32,21 @@ return new class extends Migration
         LEFT JOIN ev_log_items li
             ON l.id = li.log_id
             AND li.item_id BETWEEN 1 AND 29
-        GROUP BY l.id, l.date;
+        GROUP BY l.id, l.date)
+        SELECT
+          c.id,
+          c.odo,
+          c.soc,
+          c.ac,
+          c.ad,
+          c.lvc,
+          c.hvc,
+          c.ltc,
+          c.htc,
+          c.tc,
+          c.odo - p.odo as distance,
+          FROM ev_logs_base c
+          LEFT JOIN ev_logs_base p ON c.parent_id = p.id;
         ');
     }
 
