72e4f86791aca4928eb35f4c0d47b0b17f049727
sorethea <sorethea@gmail.com>
2025-06-22 14:33:45 +0700

    Update EvLogResource

--- app/Helpers/EvLog.php
+++ app/Helpers/EvLog.php
@@ -39,7 +39,7 @@ class EvLog
                 Select::make('parent_id')
                     ->label(trans('ev.parent'))
                     ->options(\App\Models\EvLog::select(['id','date'])->orderBy('date','desc')->get()->pluck('date','id'))
-                    ->default(fn()=>EvLog::max('id'))
+                    ->default(fn()=>\App\Models\EvLog::max('id'))
                     ->searchable(['id','date'])
                     ->nullable(),
                 Select::make("cycle_id")
@@ -48,7 +48,7 @@ class EvLog
                     ->options(\App\Models\EvLog::select(['id','date'])->where('log_type','charging')->orderBy('date','desc')->get()->pluck('date','id'))
                     //->relationship('cycle','date')
                     ->hidden(fn(Get $get)=>$get("log_type")=="charge")
-                    ->default(fn()=>EvLog::where("log_type","charging")->max('id'))
+                    ->default(fn()=>\App\Models\EvLog::where("log_type","charging")->max('id'))
                     ->searchable(['id','date'])
                     ->nullable(),
                 Select::make("charge_type")
