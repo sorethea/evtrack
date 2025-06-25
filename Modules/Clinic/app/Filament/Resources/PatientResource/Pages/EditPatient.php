de521fef14bc47d22749c115807dd95a8de59469
sorethea <sorethea@gmail.com>
2025-06-25 12:08:06 +0700

    Create Patient Resource Clinic Module

---
+++ Modules/Clinic/app/Filament/Resources/PatientResource/Pages/EditPatient.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Clinic\Filament\Resources\PatientResource\Pages;
+
+use Modules\Clinic\Filament\Resources\PatientResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPatient extends EditRecord
+{
+    protected static string $resource = PatientResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
