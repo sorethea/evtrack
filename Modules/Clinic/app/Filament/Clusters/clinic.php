a724e8a82e1858905aa71dff2113b16603131585
sorethea <sorethea@gmail.com>
2025-06-25 12:12:47 +0700

    Create Patient Resource Clinic Module

---
+++ Modules/Clinic/app/Filament/Clusters/clinic.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Clinic\Filament\Clusters;
+
+use Filament\Clusters\Cluster;
+use Nwidart\Modules\Facades\Module;
+
+class clinic extends Cluster
+{
+    public static function getModuleName(): string
+    {
+        return 'Clinic';
+    }
+
+    public static function getModule(): \Nwidart\Modules\Module
+    {
+        return Module::findOrFail(static::getModuleName());
+    }
+
+    public static function getNavigationLabel(): string
+    {
+        return __('Clinic');
+    }
+
+    public static function getNavigationIcon(): ?string
+    {
+        return 'heroicon-o-squares-2x2';
+    }
+}
