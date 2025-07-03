a6419840bfee2fa727612c0ce6287695fbdf0765
sorethea <sorethea@gmail.com>
2025-07-03 21:10:41 +0700

    add migration ev_logs_cycle_view to EV Module

--- Modules/EV/database/migrations/2025_07_03_132109_create_cycle_ev_logs_view.php
+++ Modules/EV/database/migrations/2025_07_03_132109_create_cycle_ev_logs_view.php
@@ -13,9 +13,8 @@ return new class extends Migration
     WITH ev_logs_base AS (
         SELECT
             l.id AS log_id,
-            l.cycle_id,
+            COALESCE(l.cycle_id::text, l.id::text) AS cycle_id, -- Handle NULL cycle_id
             l.vehicle_id,
-            l.parent_id,
             l.date,
             MAX(CASE WHEN li.item_id = 1 THEN li.value END) AS odo,
             MAX(CASE WHEN li.item_id = 11 THEN li.value END) AS soc,
@@ -30,65 +29,52 @@ return new class extends Migration
         LEFT JOIN ev_log_items li
             ON l.id = li.log_id
             AND li.item_id BETWEEN 1 AND 29
-        GROUP BY l.id, l.cycle_id, l.vehicle_id, l.parent_id, l.date
+        GROUP BY l.id, l.cycle_id, l.vehicle_id, l.date
     ),
-    ranked_logs AS (
-        SELECT *,
-            ROW_NUMBER() OVER (
-                PARTITION BY cycle_id ORDER BY date ASC
-            ) AS cycle_asc_rank,
-            ROW_NUMBER() OVER (
-                PARTITION BY cycle_id ORDER BY date DESC
-            ) AS cycle_desc_rank
+    cycle_data AS (
+        SELECT
+            cycle_id,
+            MIN(date) AS cycle_date,
+            MAX(date) AS end_date,
+            MIN(vehicle_id) AS vehicle_id,
+            (ARRAY_AGG( (odo, soc, ac, ad) ORDER BY date ASC))[1] AS first_rec,
+            (ARRAY_AGG( (odo, soc, ac, ad, lvc, hvc, ltc, htc, tc) ORDER BY date DESC))[1] AS last_rec
         FROM ev_logs_base
-    ),
-    cycle_roots AS (
-        SELECT
-            cycle_id, vehicle_id,
-            date AS start_date,
-            odo, soc, ac, ad
-        FROM ranked_logs
-        WHERE cycle_asc_rank = 1
-    ),
-    cycle_ends AS (
-        SELECT
-            cycle_id, vehicle_id,
-            date AS end_date,
-            odo, soc, ac, ad,
-            lvc, hvc, ltc, htc, tc
-        FROM ranked_logs
-        WHERE cycle_desc_rank = 1
+        GROUP BY cycle_id
     )
     SELECT
-        cr.cycle_id,
-        cr.vehicle_id,
-        cr.start_date,
-        ce.end_date,
-        cr.odo AS root_odo,
-        cr.soc AS root_soc,
-        cr.ac AS root_ac,
-        cr.ad AS root_ad,
-        ce.odo AS last_odo,
-        ce.soc AS last_soc,
-        ce.ac AS last_ac,
-        ce.ad AS last_ad,
-        ce.lvc AS last_lvc,
-        ce.hvc AS last_hvc,
-        ce.ltc AS last_ltc,
-        ce.htc AS last_htc,
-        ce.tc AS last_tc,
-        cr.soc - ce.soc AS soc_derivation,
-        ce.hvc - ce.lvc AS v_spread,
-        ce.htc - ce.ltc AS t_spread,
-        ce.soc - 100 * (ce.ac - ce.ad) / v.capacity AS soc_middle,
-        ce.ac - cr.ac AS charge,
-        ce.ad - cr.ad AS discharge,
-        ce.odo - cr.odo AS distance,
-        100 * ((ce.ad - cr.ad) - (ce.ac - cr.ac)) / NULLIF(ce.odo - cr.odo, 0) AS a_consumption,
-        v.capacity * (cr.soc - ce.soc) / NULLIF(ce.odo - cr.odo, 0) AS consumption
-    FROM cycle_roots cr
-    JOIN cycle_ends ce ON cr.cycle_id = ce.cycle_id
-    LEFT JOIN vehicles v ON cr.vehicle_id = v.id;');
+        cd.cycle_id,
+        cd.vehicle_id,
+        cd.cycle_date,
+        cd.end_date,
+        (cd.first_rec).odo AS root_odo,
+        (cd.first_rec).soc AS root_soc,
+        (cd.first_rec).ac AS root_ac,
+        (cd.first_rec).ad AS root_ad,
+        (cd.last_rec).odo AS last_odo,
+        (cd.last_rec).soc AS last_soc,
+        (cd.last_rec).ac AS last_ac,
+        (cd.last_rec).ad AS last_ad,
+        (cd.last_rec).lvc AS last_lvc,
+        (cd.last_rec).hvc AS last_hvc,
+        (cd.last_rec).ltc AS last_ltc,
+        (cd.last_rec).htc AS last_htc,
+        (cd.last_rec).tc AS last_tc,
+        (cd.first_rec).soc - (cd.last_rec).soc AS soc_derivation,
+        (cd.last_rec).hvc - (cd.last_rec).lvc AS v_spread,
+        (cd.last_rec).htc - (cd.last_rec).ltc AS t_spread,
+        (cd.last_rec).soc - 100 * ((cd.last_rec).ac - (cd.last_rec).ad) / v.capacity AS soc_middle,
+        (cd.last_rec).ac - (cd.first_rec).ac AS charge,
+        (cd.last_rec).ad - (cd.first_rec).ad AS discharge,
+        (cd.last_rec).odo - (cd.first_rec).odo AS distance,
+        -- Handle division by zero
+        100 * (((cd.last_rec).ad - (cd.first_rec).ad) -
+               ((cd.last_rec).ac - (cd.first_rec).ac)) /
+        NULLIF((cd.last_rec).odo - (cd.first_rec).odo, 0) AS a_consumption,
+        v.capacity * ((cd.first_rec).soc - (cd.last_rec).soc) /
+        NULLIF((cd.last_rec).odo - (cd.first_rec).odo, 0) AS consumption
+    FROM cycle_data cd
+    LEFT JOIN vehicles v ON cd.vehicle_id = v.id;');
     }
 
     public function down(): void
