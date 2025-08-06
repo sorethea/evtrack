12afc16f047db7089add4d752a809cb0eff88f49
sorethea <sorethea@gmail.com>
2025-08-06 20:59:07 +0700

    move charging cycle resource to EV Module

--- Modules/EV/database/migrations/2025_07_03_132109_create_cycle_ev_logs_view.php
+++ Modules/EV/database/migrations/2025_07_03_132109_create_cycle_ev_logs_view.php
@@ -111,6 +111,8 @@ return new class extends Migration
         (cr.root_soc - lic.last_soc) AS capacity_amp,
         100 * ((lic.last_ad - cr.root_ad) - (lic.last_ac - cr.root_ac)) /
         (cr.root_soc - lic.last_soc) AS capacity,
+        100 * ((lic.last_ada - cr.root_ada) - (lic.last_aca - cr.root_aca)) /
+        NULLIF(lic.last_odo - cr.root_odo, 0) AS a_consumption_amp,
         100 * ((lic.last_ad - cr.root_ad) - (lic.last_ac - cr.root_ac)) /
         NULLIF(lic.last_odo - cr.root_odo, 0) AS a_consumption,
         v.capacity * (cr.root_soc - lic.last_soc) /
