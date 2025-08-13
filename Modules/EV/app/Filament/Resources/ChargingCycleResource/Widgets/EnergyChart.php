a59b938d9473240c0aa2a3e12c5c0da36639539d
sorethea <sorethea@gmail.com>
2025-08-13 15:29:34 +0700

    cycle detail view

--- Modules/EV/app/Filament/Resources/ChargingCycleResource/Widgets/EnergyChart.php
+++ Modules/EV/app/Filament/Resources/ChargingCycleResource/Widgets/EnergyChart.php
@@ -12,23 +12,23 @@ class EnergyChart extends ChartWidget
 
     protected function getData(): array
     {
-        $socArray = $this->record->logs->pluck('ac')->toArray();
-        $chargeArray = $this->record->logs->pluck('ad')->toArray();
-        $dischargeArray = $this->record->logs->pluck('hvc')->toArray();
+        $socArray = $this->record->logs->pluck('soc')->toArray();
+        $chargeArray = $this->record->logs->pluck('ac')->toArray();
+        $dischargeArray = $this->record->logs->pluck('ad')->toArray();
         //$lvcArray = $this->record->logs->pluck('lvc')->toArray();
         return [
 
             'datasets'=>[
                 [
-                    'label'=>'Voltage',
+                    'label'=>'Charge',
                     'borderColor' => '#3B82F6',
                     'backgroundColor' => 'rgba(59, 130, 246, 0.2)',
                     'data'=>$chargeArray,
                 ],
                 [
-                    'label'=>'Highest',
-                    'borderColor' => '#8B5CF6',
-                    'backgroundColor' => 'rgba(139, 92, 246, 0.2)',
+                    'label'=>'Discharge',
+                    'borderColor' => '#F59E0B',
+                    'backgroundColor' => 'rgba(245, 158, 11, 0.2)',
                     'data'=>$dischargeArray,
                 ],
 //                [
