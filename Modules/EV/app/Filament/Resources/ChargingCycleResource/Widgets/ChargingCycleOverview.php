78ab14a5879825331d930d81512c895ba1389df5
sorethea <sorethea@gmail.com>
2025-08-13 08:38:30 +0700

    cycle detail view

--- Modules/EV/app/Filament/Resources/ChargingCycleResource/Widgets/ChargingCycleOverview.php
+++ Modules/EV/app/Filament/Resources/ChargingCycleResource/Widgets/ChargingCycleOverview.php
@@ -18,11 +18,18 @@ class ChargingCycleOverview extends BaseWidget
     protected function getStats(): array
     {
         $distancesArray = $this->record->logs->pluck('distance')->toArray();
+        $socArray = $this->record->logs->pluck('to_soc')->toArray();
         $from_date = Carbon::parse($this->record->cycle_date)->format('d M, Y');
         $to_date = Carbon::parse($this->record->end_date)->format('d M, Y');
 
         return [
-            Stat::make('Total distance',Number::format($this->record->distance??0).'km')
+
+            Stat::make('State of Charge',Number::format($this->record->soc??0).'km')
+                ->icon('heroicon-o-map')
+                ->color('success')
+                ->description("From {$from_date} to {$to_date}")
+                ->chart($socArray),
+            Stat::make('Distance',Number::format($this->record->distance??0).'km')
                 ->icon('heroicon-o-map')
                 ->color('success')
                 ->description("From {$from_date} to {$to_date}")
