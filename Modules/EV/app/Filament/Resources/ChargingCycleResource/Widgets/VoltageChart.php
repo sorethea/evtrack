a0c31ed985ff0d16894cb2810fcc7dff880c6abb
sorethea <sorethea@gmail.com>
2025-08-13 10:36:22 +0700

    cycle detail view

--- Modules/EV/app/Filament/Resources/ChargingCycleResource/Widgets/VoltageChart.php
+++ Modules/EV/app/Filament/Resources/ChargingCycleResource/Widgets/VoltageChart.php
@@ -8,22 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class VoltageChart extends ChartWidget
 {
     public Model $record;
-    protected static ?string $heading = 'Voltage vs. State of Charge';
+    protected static ?string $heading = 'Voltage Trench';
     protected function getData(): array
     {
         $socArray = $this->record->logs->pluck('soc')->toArray();
         $voltageArray = $this->record->logs->pluck('voltage')->toArray();
-        $distanceArray = $this->record->logs->pluck('distance')->toArray();
 
         return [
             'datasets'=>[
                 [
                     'label'=>'Voltage',
                     'data'=>$voltageArray,
-                ],
-                [
-                    'label'=>'Distance',
-                    'data'=>$distanceArray,
                 ],
             ],
             'labels'=>$socArray,
