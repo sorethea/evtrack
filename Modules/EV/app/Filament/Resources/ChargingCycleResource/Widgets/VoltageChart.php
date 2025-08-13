278cf9aa84aa06bfb8f977c46aa3cae7a3746a6c
sorethea <sorethea@gmail.com>
2025-08-13 10:09:18 +0700

    cycle detail view

--- Modules/EV/app/Filament/Resources/ChargingCycleResource/Widgets/VoltageChart.php
+++ Modules/EV/app/Filament/Resources/ChargingCycleResource/Widgets/VoltageChart.php
@@ -3,9 +3,11 @@
 namespace Modules\EV\Filament\Resources\ChargingCycleResource\Widgets;
 
 use Filament\Widgets\ChartWidget;
+use Illuminate\Database\Eloquent\Model;
 
 class VoltageChart extends ChartWidget
 {
+    public Model $record;
     protected static ?string $heading = 'Voltage vs. State of Charge';
 
     protected function getData(): array
