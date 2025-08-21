1b95f5e651380c5c5f00e84a2fc73cbdcfb72bbc
sorethea <sorethea@gmail.com>
2025-08-21 15:59:02 +0700

    EvLog Analyse

--- Modules/EV/app/Filament/Resources/EvLogResource/Widgets/AnalyseEvLogOverview.php
+++ Modules/EV/app/Filament/Resources/EvLogResource/Widgets/AnalyseEvLogOverview.php
@@ -13,27 +13,32 @@ class AnalyseEvLogOverview extends BaseWidget
     //protected ?string $heading = 'Overview';
     protected function getStats(): array
     {
-        $socArray = $this->record->cycleView->logs->pluck('soc')->toArray();
-        $distancesArray = $this->record->cycleView->logs->pluck('distance')->toArray();
-        $consumptionArray = $this->record->cycleView->logs->pluck('consumption')->toArray();
-        $cycleConsumption = Number::format($this->record->cycleView->consumption*10,0);
-        $cycleDistance = Number::format($this->record->cycleView->distance,1);
-        return [
-            Stat::make('Current SoC',Number::format($this->record->detail->soc??0).'%')
-                ->icon('heroicon-o-battery-50')
-                ->color('danger')
-                ->description("Cycle SoC from {$this->record->cycleView->root_soc}% to {$this->record->cycleView->last_soc}%")
-                ->chart($socArray),
-            Stat::make('Consumption',Number::format($this->record->detail->consumption*10,0).' Wh/km')
-                ->icon('heroicon-o-bolt')
-                ->color('warning')
-                ->description("Cycle consumption: {$cycleConsumption} Wh/km")
-                ->chart($consumptionArray),
-            Stat::make('Distance',Number::format($this->record->detail->distance??0).'km')
-                ->icon('heroicon-o-map')
-                ->color('success')
-                ->description("Cycle distance: {$cycleDistance} km")
-                ->chart($distancesArray),
-        ];
+        if($this->record->log_type=='driving'){
+
+            $socArray = $this->record->cycleView->logs->pluck('soc')->toArray();
+            $distancesArray = $this->record->cycleView->logs->pluck('distance')->toArray();
+            $consumptionArray = $this->record->cycleView->logs->pluck('consumption')->toArray();
+            $cycleConsumption = Number::format($this->record->cycleView->consumption*10,0);
+            $cycleDistance = Number::format($this->record->cycleView->distance,1);
+
+            return [
+                Stat::make('Current SoC',Number::format($this->record->detail->soc??0).'%')
+                    ->icon('heroicon-o-battery-50')
+                    ->color('danger')
+                    ->description("Cycle SoC from {$this->record->cycleView->root_soc}% to {$this->record->cycleView->last_soc}%")
+                    ->chart($socArray),
+                Stat::make('Consumption',Number::format($this->record->detail->consumption*10,0).' Wh/km')
+                    ->icon('heroicon-o-bolt')
+                    ->color('warning')
+                    ->description("Cycle consumption: {$cycleConsumption} Wh/km")
+                    ->chart($consumptionArray),
+                Stat::make('Distance',Number::format($this->record->detail->distance??0).'km')
+                    ->icon('heroicon-o-map')
+                    ->color('success')
+                    ->description("Cycle distance: {$cycleDistance} km")
+                    ->chart($distancesArray),
+            ];
+        }
+        return [];
     }
 }
