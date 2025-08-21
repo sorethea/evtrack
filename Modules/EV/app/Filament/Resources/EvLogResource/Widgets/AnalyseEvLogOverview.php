26d9ac5e79a5d551a2dcda1c9c8c4b6fcf562deb
sorethea <sorethea@gmail.com>
2025-08-21 15:48:37 +0700

    EvLog Analyse

--- Modules/EV/app/Filament/Resources/EvLogResource/Widgets/AnalyseEvLogOverview.php
+++ Modules/EV/app/Filament/Resources/EvLogResource/Widgets/AnalyseEvLogOverview.php
@@ -16,6 +16,8 @@ class AnalyseEvLogOverview extends BaseWidget
         $socArray = $this->record->cycleView->logs->pluck('soc')->toArray();
         $distancesArray = $this->record->cycleView->logs->pluck('distance')->toArray();
         $consumptionArray = $this->record->cycleView->logs->pluck('consumption')->toArray();
+        $cycleConsumption = Number::format($this->record->cycleView->consumption,0);
+        $cycleDistance = Number::format($this->record->cycleView->distance,1);
         return [
             Stat::make('Current SoC',Number::format($this->record->detail->soc??0).'%')
                 ->icon('heroicon-o-battery-50')
@@ -25,11 +27,12 @@ class AnalyseEvLogOverview extends BaseWidget
             Stat::make('Consumption',Number::format($this->record->detail->consumption*10,0).' Wh/km')
                 ->icon('heroicon-o-bolt')
                 ->color('warning')
-                ->description("Cycle consumption: {$this->record->cycleView->consumption} Wh/km")
+                ->description("Cycle consumption: {$cycleConsumption} Wh/km")
                 ->chart($consumptionArray),
             Stat::make('Distance',Number::format($this->record->detail->distance??0).'km')
                 ->icon('heroicon-o-map')
                 ->color('success')
+                ->description("Cycle distance: {$cycleDistance} km")
                 ->chart($distancesArray),
         ];
     }
