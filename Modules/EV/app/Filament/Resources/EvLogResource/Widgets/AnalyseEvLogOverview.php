7f2bb19abab6e5e8d576f7b61fc155e91cbd032f
sorethea <sorethea@gmail.com>
2025-08-21 15:39:43 +0700

    EvLog Analyse

--- Modules/EV/app/Filament/Resources/EvLogResource/Widgets/AnalyseEvLogOverview.php
+++ Modules/EV/app/Filament/Resources/EvLogResource/Widgets/AnalyseEvLogOverview.php
@@ -14,12 +14,23 @@ class AnalyseEvLogOverview extends BaseWidget
     protected function getStats(): array
     {
         $socArray = $this->record->cycleView->logs->pluck('soc')->toArray();
+        $distancesArray = $this->record->cycleView->logs->pluck('distance')->toArray();
+        $consumptionArray = $this->record->cycleView->logs->pluck('consumption')->toArray();
         return [
             Stat::make('Current SoC',Number::format($this->record->detail->soc??0).'%')
                 ->icon('heroicon-o-battery-50')
                 ->color('danger')
                 ->description("Battery from {$this->record->cycleView->root_soc}% to {$this->record->detail->soc}%")
                 ->chart($socArray),
+            Stat::make('Consumption',Number::format($this->record->detail->consumption*10,0).' Wh/km')
+                ->icon('heroicon-o-bolt')
+                ->color('warning')
+                ->description("Gross discharge: {$this->record->detaile->discharge} kWh & Regenerative Braking: {$this->record->detail->charge} kWh")
+                ->chart($consumptionArray),
+            Stat::make('Distance',Number::format($this->record->detail->distance??0).'km')
+                ->icon('heroicon-o-map')
+                ->color('success')
+                ->chart($distancesArray),
         ];
     }
 }
