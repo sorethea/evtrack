e755585214c4a74c71a171696b0a937a36055775
sorethea <sorethea@gmail.com>
2025-08-22 13:55:09 +0700

    EvLog Analyse

--- Modules/EV/app/Filament/Resources/EvLogResource.php
+++ Modules/EV/app/Filament/Resources/EvLogResource.php
@@ -272,17 +272,17 @@ class EvLogResource extends Resource
                     ->summarize(Tables\Columns\Summarizers\Sum::make()->label(trans('ev.distance'))),
             ])
             //->defaultGroup('cycle.date')
-            ->groups([
-                Tables\Grouping\Group::make('cycle.date')->date()
-//                    ->orderQueryUsing(
-//                        fn (Builder $query, string $direction) => $query
-//                            ->selectRaw('COALESCE(cycle.date, ev_logs.date) as group_date')
-//                            ->leftJoin('ev_logs as cycle', 'ev_logs.cycle_id', '=', 'cycle.id')
-//                            ->orderBy('group_date', $direction),
-//                        'desc' // Default to descending
-//                    )
-            ])
-            ->defaultSort('cycle.date','desc')
+//            ->groups([
+//                Tables\Grouping\Group::make('cycle.date')->date()
+////                    ->orderQueryUsing(
+////                        fn (Builder $query, string $direction) => $query
+////                            ->selectRaw('COALESCE(cycle.date, ev_logs.date) as group_date')
+////                            ->leftJoin('ev_logs as cycle', 'ev_logs.cycle_id', '=', 'cycle.id')
+////                            ->orderBy('group_date', $direction),
+////                        'desc' // Default to descending
+////                    )
+//            ])
+//            ->defaultSort('cycle.date','desc')
             ->filters([
                 Tables\Filters\QueryBuilder::make()
                     ->constraints([
