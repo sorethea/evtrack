bfea3821a76fb97b299ae54e29c1ef62bfbfb34f
sorethea <sorethea@gmail.com>
2025-08-22 11:00:19 +0700

    EvLog Analyse

--- Modules/EV/app/Filament/Resources/EvLogResource/Widgets/AnalyseEvLogOverview.php
+++ Modules/EV/app/Filament/Resources/EvLogResource/Widgets/AnalyseEvLogOverview.php
@@ -67,6 +67,11 @@ class AnalyseEvLogOverview extends BaseWidget
                     ->color(Color::Purple)
                     ->description("Cycle net energy used: {$cycleNetDischarge} kWh")
                     ->chart($netEnergyArray),
+                Stat::make('Estimated Energy To 100%',Number::format($cycleNetDischarge,0).'kWh')
+                    ->icon('custom-battery')
+                    ->color(Color::Gray)
+                    ->description("Cycle net energy used: {$cycleNetDischarge} kWh")
+                    ->chart($netEnergyArray),
             ];
         }
         return [];
