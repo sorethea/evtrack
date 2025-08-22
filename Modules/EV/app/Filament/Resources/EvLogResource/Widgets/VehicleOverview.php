46006dad1d0843b2dcfc8f0701916b5e6781e5ac
sorethea <sorethea@gmail.com>
2025-08-22 17:46:01 +0700

    EvLog Analyse

--- Modules/EV/app/Filament/Resources/EvLogResource/Widgets/VehicleOverview.php
+++ Modules/EV/app/Filament/Resources/EvLogResource/Widgets/VehicleOverview.php
@@ -2,6 +2,7 @@
 
 namespace Modules\EV\Filament\Resources\EvLogResource\Widgets;
 
+use Filament\Support\Colors\Color;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Number;
@@ -17,7 +18,9 @@ class VehicleOverview extends BaseWidget
         $ac = \evlog::getItemValue($log,19);
         $ad = \evlog::getItemValue($log,20);
         return [
-            Stat::make(trans('ev.odo'),Number::format($odo).'km'),
+            Stat::make(trans('ev.odo'),Number::format($odo).'km')
+                ->icon('custom-distance')
+                ->color(Color::Green),
             Stat::make(trans('ev.soc'),Number::format($soc).'%'),
             Stat::make(trans('ev.accumulative').' '.trans('ev.charge'),Number::format($ac).'kWh'),
             Stat::make(trans('ev.accumulative').' '.trans('ev.discharge'),Number::format($ad).'kWh'),
