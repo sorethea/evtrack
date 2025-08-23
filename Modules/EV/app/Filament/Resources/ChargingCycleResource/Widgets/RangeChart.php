a209af0aee07afb1aa4225bdb9ac5c6edbe6ea65
sorethea <sorethea@gmail.com>
2025-08-23 20:42:23 +0700

    EvLog Analyse

--- Modules/EV/app/Filament/Resources/ChargingCycleResource/Widgets/RangeChart.php
+++ Modules/EV/app/Filament/Resources/ChargingCycleResource/Widgets/RangeChart.php
@@ -2,6 +2,7 @@
 
 namespace Modules\EV\Filament\Resources\ChargingCycleResource\Widgets;
 
+use Filament\Support\Colors\Color;
 use Filament\Widgets\ChartWidget;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,6 +13,7 @@ class RangeChart extends ChartWidget
 
     protected function getData(): array
     {
+        $socArray = $this->record->logs->pluck('soc')->toArray();
         $rangeArray = $this->record->logs->pluck('range')->toArray();
         $capacityArray = $this->record->logs->pluck('capacity')->toArray();
         return [
@@ -19,14 +21,14 @@ class RangeChart extends ChartWidget
             'datasets'=>[
                 [
                     'label'=>'Range',
-                    'borderColor' => '#8B5CF6',
-                    'backgroundColor' => 'rgba(139, 92, 246, 0.2)',
+                    'borderColor' => 'success',
+                    'backgroundColor' => Color::Teal,
                     'data'=>$rangeArray,
                 ],
                 [
-                    'label'=>'Capacity',
-                    'borderColor' => '#F59E0B',
-                    'backgroundColor' => 'rgba(245, 158, 11, 0.2)',
+                    'label'=>'Range',
+                    'borderColor' => 'warning',
+                    'backgroundColor' => Color::Yellow,
                     'data'=>$capacityArray,
                 ],
 //                [
