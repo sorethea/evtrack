7857265930f87f8cc6336205a7c2929f6747ba10
sorethea <sorethea@gmail.com>
2025-08-23 09:36:36 +0700

    Vehicle Overview

--- Modules/EV/app/Filament/Resources/EvLogResource/Widgets/VehicleOverview.php
+++ Modules/EV/app/Filament/Resources/EvLogResource/Widgets/VehicleOverview.php
@@ -16,6 +16,7 @@ class VehicleOverview extends BaseWidget
         $distance = $log->cycleView->distance;
         $cycleDistanceArray = $log->cycleView->logs->pluck('distance')->toArray();
         $soc = $log->detail->soc;
+        $remainRange = 100*$distance/$soc;
         $cycleSoCArray = $log->cycleView->logs->pluck('soc')->toArray();
         $ac =  $log->detail->ac;
         $ad =  $log->detail->ad;
@@ -24,6 +25,7 @@ class VehicleOverview extends BaseWidget
             Stat::make(trans('ev.distance'),Number::format($distance).'km')
                 //->icon('custom-location-color-bookmark-add')
                 ->color(Color::Green)
+                ->description('Estimated remaining range: '.Number::format($remainRange,1).' km')
                 ->chart($cycleDistanceArray),
             Stat::make(trans('ev.soc'),Number::format($soc).'%')
                 //->icon('custom-percentage')
