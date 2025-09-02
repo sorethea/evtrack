416ac3a4227b4c8f7ae7ba80814a060f90c18901
sorethea <sorethea@gmail.com>
2025-09-02 18:49:38 +0700

    Charging Cycle Dashbaord

--- Modules/EV/app/Helpers/EvLog.php
+++ Modules/EV/app/Helpers/EvLog.php
@@ -87,10 +87,10 @@ class EvLog
                 ->description( "Capacity variant: {$capacityVariant}%")
                 ->chart($cycleCapacityArray)
                 ->color(Color::Pink),
-//            Stat::make(trans('ev.voltage'),Number::format($deltaVoltage,0).'mV')
-//                ->description( "Highest cell: {$log?->cycleView?->last_hvc}V. Lowest cell: {$log?->cycleView?->last_lvc}V.")
-//                ->chart($cycleHCVArray)
-//                ->color(Color::Orange),
+            Stat::make(trans('ev.voltage'),Number::format($deltaVoltage,0).'mV')
+                ->description( "Highest cell: {$log?->cycleView?->last_hvc}V. Lowest cell: {$log?->cycleView?->last_lvc}V.")
+                ->chart($cycleHCVArray)
+                ->color(Color::Orange),
 //            Stat::make(trans('ev.temperature'),Number::format($deltaTemp,0).'C')
 //                ->description( "Highest cell: {$log?->cycleView?->last_htc}V. Lowest cell: {$log?->cycleView?->last_ltc}V.")
 //                ->chart($cycleHTVArray)
