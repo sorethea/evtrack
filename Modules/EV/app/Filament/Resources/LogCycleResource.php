85b76b96efad97a1247880cae37fa2a367b06df6
sorethea <sorethea@gmail.com>
2026-02-21 11:14:41 +0700

    Add new LogCycleResource

--- Modules/EV/app/Filament/Resources/LogCycleResource.php
+++ Modules/EV/app/Filament/Resources/LogCycleResource.php
@@ -37,7 +37,7 @@ class LogCycleResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                //Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
