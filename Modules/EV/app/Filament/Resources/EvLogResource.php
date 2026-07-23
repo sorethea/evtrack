6dafe650ae0f5ac2f5f740bf57dc93e3b808a205
sorethea <sorethea@gmail.com>
2026-07-23 10:22:35 +0700

    Update cycle_ev_logs_view

--- Modules/EV/app/Filament/Resources/EvLogResource.php
+++ Modules/EV/app/Filament/Resources/EvLogResource.php
@@ -120,7 +120,7 @@ class EvLogResource extends Resource
                     ->date('d M, Y H:i')
                     ->searchable(),
                 Tables\Columns\TextColumn::make("duration")
-                    ->getStateUsing(fn($record)=>gmdate("H:i",Carbon::make($record?->parent?->date)->diffInSeconds($record?->date)),
+                    ->getStateUsing(fn($record)=>gmdate("H:i",Carbon::make($record?->parent?->date??0)->diffInSeconds($record?->date??0)),
                 Tables\Columns\TextColumn::make("log_type")
                     ->badge()
                     ->color(fn(string $state) => match ($state) {
