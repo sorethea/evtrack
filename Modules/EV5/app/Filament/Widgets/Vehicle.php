e7dceb64db549e99cd9bea2197bf83191587a10f
sorethea <sorethea@gmail.com>
2026-08-07 11:08:59 +0700

    Add EV5 module

--- Modules/EV5/app/Filament/Widgets/Vehicle.php
+++ Modules/EV5/app/Filament/Widgets/Vehicle.php
@@ -12,9 +12,9 @@ class Vehicle extends Widget
     protected string $view = 'ev5::filament.widgets.vehicle';
     public string $header = "My Vehicle";
     public object $vehicle;
-    public function mount()
+    public function mount(): void
     {
-        $this->vehicle = auth()->user()->vehicle->where('is_default',true);
+        $this->vehicle = auth()->user()->vehicle->where('is_default',true)->get();
     }
 
 }
