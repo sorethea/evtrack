340c2717d8f840b2037493bda6301c141f4e7084
sorethea <sorethea@gmail.com>
2026-08-07 10:44:22 +0700

    Add EV5 module

--- Modules/EV5/resources/views/filament/widgets/vehicle.blade.php
+++ Modules/EV5/resources/views/filament/widgets/vehicle.blade.php
@@ -1,7 +1,7 @@
 <x-filament-widgets::widget>
     <x-filament::section>
         <div class="fi-filament-info-widget">
-            <h2 class="fi-header">
+            <h2 class="fi-account-widget-heading">
                 {!! $header !!}
             </h2>
         </div>
