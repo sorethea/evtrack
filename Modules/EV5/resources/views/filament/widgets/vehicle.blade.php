16bd108f13d3d353996fdc378ee8022158a071e3
sorethea <sorethea@gmail.com>
2026-08-07 12:08:50 +0700

    Ev5 Vehicle Widget

--- Modules/EV5/resources/views/filament/widgets/vehicle.blade.php
+++ Modules/EV5/resources/views/filament/widgets/vehicle.blade.php
@@ -7,9 +7,12 @@
             <p class="fi-account-widget-user-name">
                 {!! $vehicle->make !!} &nbsp; {!! $vehicle->model !!} &nbsp; {!! $vehicle->year !!}
             </p>
-            <div class="fi-ta-row">
-                <p class="fi-ta-cell"><label class="fi-ta-cell-label">{{trans("ev5::ev.odo")}}:&nbsp;</label><span>{{$vehicle->odo}}km</span></p>
-                <p class="fi-ta-cell"><label class="fi-ta-cell-label">{{trans("ev5::ev.soc")}}:&nbsp;</label><span>{{$vehicle->soc}}%</span></p>
+            <div class="fi-ta-table">
+                <div class="fi-ta-row">
+                    <p class="fi-ta-cell"><label class="fi-ta-cell-label">{{trans("ev5::ev.odo")}}:&nbsp;</label><span>{{$vehicle->odo}}km</span></p>
+                    <p class="fi-ta-cell"><label class="fi-ta-cell-label">{{trans("ev5::ev.soc")}}:&nbsp;</label><span>{{$vehicle->soc}}%</span></p>
+                </div>
+
             </div>
 
         </div>
