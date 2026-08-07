61052cc6cea417e007ea410d8c4436e3e2225529
sorethea <sorethea@gmail.com>
2026-08-07 12:07:34 +0700

    Ev5 Vehicle Widget

--- Modules/EV5/resources/views/filament/widgets/vehicle.blade.php
+++ Modules/EV5/resources/views/filament/widgets/vehicle.blade.php
@@ -9,7 +9,7 @@
             </p>
             <div class="fi-ta-row">
                 <p class="fi-ta-cell"><label class="fi-ta-cell-label">{{trans("ev5::ev.odo")}}:&nbsp;</label><span>{{$vehicle->odo}}km</span></p>
-                <p class="fi-ta-cell"><label class="fi-ta-cell-label">{{trans("ev5::ev.soc")}}</label>:&nbsp;{{$vehicle->soc}}%</p>
+                <p class="fi-ta-cell"><label class="fi-ta-cell-label">{{trans("ev5::ev.soc")}}:&nbsp;</label><span>{{$vehicle->soc}}%</span></p>
             </div>
 
         </div>
