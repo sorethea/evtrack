a28ba23e737e73fdce1d8a65fae679b6e18498c5
sorethea <sorethea@gmail.com>
2026-08-12 20:24:41 +0700

    Update EVlog resource

--- Modules/EV/app/Filament/Resources/EvLogResource.php
+++ Modules/EV/app/Filament/Resources/EvLogResource.php
@@ -46,7 +46,6 @@ class EvLogResource extends Resource
     {
         return $form
             ->schema([
-                Section::make([
                     DateTimePicker::make("date")
                         ->label(trans('ev.date'))
                         ->default(now()->format('Y-m-d H i'))
@@ -126,8 +125,6 @@ class EvLogResource extends Resource
                         ->label(trans('ev.remark'))
                         ->columnSpan(2)
                         ->nullable(),
-
-                ])
             ]);
     }
 
