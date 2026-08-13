28bd81b041eda6a8bd11f2fc2ae4d4de84a58fd9
sorethea <sorethea@gmail.com>
2026-08-13 10:57:11 +0700

    Update ev cycle logs view migration

--- Modules/EV/database/migrations/2025_06_18_012713_create_ev_logs_view.php
+++ Modules/EV/database/migrations/2025_06_18_012713_create_ev_logs_view.php
@@ -46,7 +46,7 @@ return new class extends Migration
         c.cycle_id,
         c.date,
         c.odo,
-        c.consumption,
+        c.consumption AS consume,
         c.voltage,
         c.soc,
         c.aca,
@@ -77,16 +77,16 @@ return new class extends Migration
             ELSE 0
         END AS `soh`,
         CASE
-            WHEN `c`.consumption >0 THEN
-                (`c`.`odo` - `p`.`odo`)*`c`.consumption/100
+            WHEN `c`.consume >0 THEN
+                (`c`.`odo` - `p`.`odo`)*`c`.consume/100
             ELSE
                 0
         END AS base_used_energy,
         CASE
-            WHEN `c`.consumption >0 THEN
+            WHEN `c`.consume >0 THEN
                 (`c`.`ad` - ifnull( `p`.`ad`, 0 ) - (
             `c`.`ac` - ifnull( `p`.`ac`, 0 )))-
-                (`c`.`odo` - `p`.`odo`)*`c`.consumption/100
+                (`c`.`odo` - `p`.`odo`)*`c`.consume/100
             ELSE
                 0
         END AS lost_energy,
