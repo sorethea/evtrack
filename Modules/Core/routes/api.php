d740b28bc53f0140108ac8508e867376575708e1
sorethea <sorethea@gmail.com>
2026-08-14 19:34:10 +0700

    DeleteCore System Module

--- Modules/Core/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Core\Http\Controllers\CoreController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('cores', CoreController::class)->names('core');
-});
