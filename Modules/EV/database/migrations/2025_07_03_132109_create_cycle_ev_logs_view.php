374fa653614b4c22ac5d002cb0c043429288617a
sorethea <sorethea@gmail.com>
2026-08-14 11:10:30 +0700

    Update ev cycle logs view migration

--- Modules/EV/database/migrations/2025_07_03_132109_create_cycle_ev_logs_view.php
+++ Modules/EV/database/migrations/2025_07_03_132109_create_cycle_ev_logs_view.php
@@ -316,7 +316,6 @@ cycle_roots AS (
     SELECT
         b1.cycle_id,
         b1.vehicle_id,
-        b1.parent_id,
         b1.date AS cycle_date,
         b1.odo AS root_odo,
         b1.voltage AS root_voltage,
@@ -335,7 +334,6 @@ cycle_roots AS (
 last_in_cycle AS (
     SELECT
         b2.cycle_id,
-        b2.parent_id,
         b2.date AS end_date,
         b2.odo AS last_odo,
         b2.soc AS last_soc,
