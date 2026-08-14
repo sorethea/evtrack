d4ea6c67598f592bb78499d03b4a45af6b5d05c6
sorethea <sorethea@gmail.com>
2026-08-14 11:27:28 +0700

    Update ev cycle logs view migration

--- Modules/EV/app/Filament/Resources/ChargingCycleResource/RelationManagers/LogsRelationManager.php
+++ Modules/EV/app/Filament/Resources/ChargingCycleResource/RelationManagers/LogsRelationManager.php
@@ -68,13 +68,11 @@ class LogsRelationManager extends RelationManager
                     ->label(trans("ev.soh")),
                 Tables\Columns\ColumnGroup::make('SoC(%)',[
                     Tables\Columns\TextColumn::make('log.parent.soc')
-                        ->inverseRelationship('log')
                         ->numeric(1)
                         ->label(trans('ev.from') )
                         ->toggleable(isToggledHiddenByDefault: false),
                     //->summarize(Tables\Columns\Summarizers\Summarizer::make()->using(fn(\Illuminate\Database\Query\Builder $query)=>$query->max('parent_soc'))),
                     Tables\Columns\TextColumn::make('soc')
-                        ->inverseRelationship('log')
                         ->numeric(1)
                         ->label(trans('ev.to') )
                         ->toggleable(isToggledHiddenByDefault: false),
