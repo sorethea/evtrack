384d05773d517fd86b05b3b4d70d279e4f98344d
sorethea <sorethea@gmail.com>
2026-08-14 10:56:30 +0700

    Update ev cycle logs view migration

--- Modules/EV/app/Models/ChargingCycle.php
+++ Modules/EV/app/Models/ChargingCycle.php
@@ -18,7 +18,7 @@ class ChargingCycle extends Model
 
     public function parent():BelongsTo
     {
-        return $this->belongsTo(EvLog::class,'id','id');
+        return $this->belongsTo(EvLog::class,'parent_id','id');
     }
     public function vehicle():BelongsTo
     {
